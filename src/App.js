4526808eed49738ee59f0c18e51fc99081024352
suryateja1122 <suryatejachintapalli147@gmail.com>
2021-11-23 22:15:13 +0530

    worked on hoc and prop pattren

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import BasicClassComp from './components/BasicClassComp'
 import ParentComponent from './components/ParentComponent'
 import BasicFormHandling from './components/BasicFormHandling'
 import HTTPRequest from './components/HTTPRequest'
-
+import RefsDemo from './components/RefsDemo'
+import FocusInput from './components/FocusInput'
+import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCounter'
 function App() {
   return (
     <div className='App'>
       {/* <BasicFunctionComp /> */}
       {/* <BasicClassComp name='Surya' /> */}
-      <HTTPRequest/>
+      {/* <HTTPRequest/> */}
+      {/* <RefsDemo/> */}
+      {/* <FocusInput/> */}
+      <ClickCounter />
+      <HoverCounter />
     </div>
   )
 }
